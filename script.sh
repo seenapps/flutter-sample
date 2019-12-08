22df939c802439a73d9958e4c1e2514fe4217728
emedhermrsool <emedher@mrsool.co>
2019-12-08 10:04:17 +0300

    up

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+rm -R vendor/inject.dart
 git clone https://github.com/cingulo/inject.dart.git vendor/inject.dart
 flutter packages pub run build_runner build --build-filter "lib/data/*/*.dart" --delete-conflicting-outputs
 flutter packages pub run build_runner build --build-filter "lib/app/*.dart" --delete-conflicting-output
